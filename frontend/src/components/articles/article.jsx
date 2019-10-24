79350b030a1ee048fb818238a5e6232c3fcbbbee
John Steen <john@johnsteen.co>
2019-10-24 17:36:24 -0500

    Add styling to article editor and correct scroll behavior

--- frontend/src/components/articles/article.jsx
+++ frontend/src/components/articles/article.jsx
@@ -20,22 +20,25 @@ export default class Article extends Component {
 
     render() {
         return (
-            <div>
-                <form className="title-text-editor">
-                    <input 
-                        type="text"
-                        value={this.state.title}
-                        onChange={this.update('title')}
-                        placeholder="Title"
-                    />
-                </form>
-                <div>
-                    <ArticleEditor 
-                        handlePost={this.props.composeArticle} 
-                        placeholder="Share your thoughts..."
-                        author={this.props.author}
-                        title={this.state.title}
-                    />
+            <div className="compose-article-outer">
+                <div className="compose-article-inner">
+                    <form className="title-text-editor">
+                        <input 
+                            type="text"
+                            value={this.state.title}
+                            onChange={this.update('title')}
+                            placeholder="Title"
+                        />
+                    </form>
+                    <div>
+                        <ArticleEditor 
+                            handlePost={this.props.composeArticle} 
+                            placeholder="Share your thoughts..."
+                            author={this.props.author}
+                            title={this.state.title}
+                        />
+                    </div>
+
                 </div>
             </div>
         )
