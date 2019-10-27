3b95939e3c7557fba6c38cde3b773e9f8c736a6b
Han Kim <joshhk72@gmail.com>
2019-10-26 23:28:11 -0400

    Update backend image upload to aws s3

--- routes/api/images.js
+++ routes/api/images.js
@@ -6,8 +6,11 @@ const singleUpload = upload.single('image');
 
 router.post('/upload', (req, res) => {
   singleUpload(req, res, err => {
-    return res.json({'imageUrl': req.file.location})
+    if (err) { 
+      return res.status(404).send({ errors: [{ title: 'File upload error', detail: err.message }] });
+    };
+    return res.json({'imageUrl': req.file.location});
   });
 });
 
-module.exports(router);
+module.exports = router;
--- services/file-upload.js
+++ services/file-upload.js
@@ -11,10 +11,20 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid Mime Type, only JPEG and PNG'), false);
+  }
+};
+
 const upload = multer({
+  fileFilter,
   storage: multerS3({
-    s3: s3,
+    s3,
     bucket: 'thesuperapp',
+    acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
