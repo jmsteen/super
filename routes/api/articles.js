7896503fadbb95afc15571de5bf324a1aaeb0f28
Han Kim <joshhk72@gmail.com>
2019-10-29 12:10:07 -0400

    Fixed issue where likes were not being populated

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -22,6 +22,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   Article.findById(req.params.id)
+    .populate('likes')
     .populate({ path: "comments", populate: { path: "likes" } })
     .populate({ path: "author", populate: { path: "follows" }})
     .then(article => {
