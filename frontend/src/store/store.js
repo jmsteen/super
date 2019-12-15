bd7f43056e7c5b2517b237007e5b018a08e56392
Han Kim <joshhk72@gmail.com>
2019-12-14 22:05:28 -0500

    Comment out redux-logger import to allow pushing to heroku

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,17 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-
-let logger;
-if (NODE_ENV !== 'production') {
-    logger = require('redux-logger');
-}
+// import logger from 'redux-logger';
 
 const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk, logger)
+        applyMiddleware(thunk)
     )
 );
 
